5a296c17eebf8ceb3361ed04bfc070c353c49803
t895 <clombardo169@gmail.com>
2024-04-19 13:54:36 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,20 +37,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Proxy stuff
     implementation("org.pcap4j:pcap4j-core:1.8.2")
     implementation("org.pcap4j:pcap4j-packetfactory-static:1.8.2")
     implementation("dnsjava:dnsjava:3.0.0")
 
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.code.gson:gson:2.10.1")
 
-    testImplementation("junit:junit:4.13.1")
-    testImplementation("org.mockito:mockito-core:1.10.19")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:4.6.1")
     testImplementation ("org.powermock:powermock-api-mockito:1.6.6") {
         exclude(module = "hamcrest-core")
         exclude(module = "objenesis")
